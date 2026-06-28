5151648ae29c4766e6c0dd764c0ec6ad5c67b359
munonguyen <munonguyen2005@gmail.com>
2026-06-29 06:59:46 +0700

    Fix building assignment payload

--- src/main/java/com/devon/building/model/dto/AssignBuildingDTO.java
+++ src/main/java/com/devon/building/model/dto/AssignBuildingDTO.java
@@ -1,6 +1,5 @@
 package com.devon.building.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
